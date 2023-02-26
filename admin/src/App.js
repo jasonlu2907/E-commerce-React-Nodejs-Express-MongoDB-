8056fe9ebf1fec3e35e9dc3e325ba0e4deaf7d7e
Jason Lu <jasonlu2907@gmail.com>
2023-02-25 23:13:37 -0600

    Admin dashboard with redux

--- admin/src/App.js
+++ admin/src/App.js
@@ -12,23 +12,31 @@ import NewUser from './pages/new-user/NewUser';
 import ProductList from './pages/productList/ProductList';
 import Product from './pages/product/Product';
 import NewProduct from './pages/new-product/NewProduct';
+import Login from './pages/login/Login';
+
+import { useSelector } from 'react-redux';
 
 function App() {
+  const admin = useSelector((state) => state.user.currentUser.isAdmin);
+
   return (
     <BrowserRouter>
       <Topbar />
-      <div className='container'>
-        <Sidebar />
-        <Routes>
-          <Route exact path='/' element={<Home />} />
-          <Route path='/users' element={<UserList />} />
-          <Route path='/user/:userId' element={<User />} />
-          <Route path='/newUser' element={<NewUser />} />
-          <Route path='/products' element={<ProductList />} />
-          <Route path='/product/:prodId' element={<Product />} />
-          <Route path='/newProduct' element={<NewProduct />} />
-        </Routes>
-      </div>
+      {admin && (
+        <div className='container'>
+          <Sidebar />
+          <Routes>
+            <Route exact path='/' element={<Home />} />
+            <Route path='/users' element={<UserList />} />
+            <Route path='/user/:userId' element={<User />} />
+            <Route path='/newUser' element={<NewUser />} />
+            <Route path='/products' element={<ProductList />} />
+            <Route path='/product/:prodId' element={<Product />} />
+            <Route path='/newProduct' element={<NewProduct />} />
+            <Route path='/login' element={<Login />} />
+          </Routes>
+        </div>
+      )}
     </BrowserRouter>
   );
 }
---
+++ admin/src/requestMethods.js
@@ -0,0 +1,18 @@
+import axios from 'axios';
+
+//OLD LOCAL URL
+const URL = `http://localhost:4000/api/v1`;
+// const URL = `https://siteecommerceapi.herokuapp.com/api/v1`;
+const TOKEN = JSON.parse(JSON.parse(localStorage.getItem('persist:root')).user)
+  .currentUser.accessToken;
+  
+// NON-USER searching
+export const publicRequest = axios.create({
+  baseURL: URL,
+});
+
+// USER searching
+export const userRequest = axios.create({
+  baseURL: URL,
+  header: { token: `Bearer ${TOKEN}` },
+});
